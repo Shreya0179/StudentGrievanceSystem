4e20546d924927099643957725c3fd477b27a830
Shreya <shreya.2428cseaiml25@kiet.edu>
2026-04-23 16:06:12 +0530

    Fixed API base URL with env

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://studentgrievancesystem-upw9.onrender.com/"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 API.interceptors.request.use((req) => {
